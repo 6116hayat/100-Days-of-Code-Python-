5e8ef1632e7794267e4e9c9a490fcf75b70f3074
Umar Hayat <umarhayat9718@gmail.com>
2024-01-26 09:45:58 +0530

    This is the first Commit

---
+++ Begginner/Day 0.py
@@ -0,0 +1,5 @@
+# Printing Statements in different lines with single print
+print('Hello World \nHello World')
+
+# Using String Concatenation to print the statements
+print('Hello' +' '+ 'World')
---
+++ Begginner/Day 1.py
@@ -0,0 +1,33 @@
+# printing nested code
+'''
+print('Hello ' + input('What is your Name' + '?'))
+'''
+
+# printing the length of the entered name or character
+'''
+char = input('Enter your name or any character of your choice ?')
+print(len(char))
+'''
+
+# Switching values of 2 Variables
+'''
+a = input('Enter the value of a:')
+b = input('Enter the value of b:')
+c = a
+a = b
+b = c
+print('a:' + str(a))
+print('b:' + str(b))
+'''
+
+# Project : Band Name Generator
+'''
+print('Welcome to the Band Name Generator ')
+city = input('Enter the city you grew up in ?? \n')
+# print(city)
+petName = input('What is your pets name ?? \n')
+# print(petName)
+
+print('Your Band Name is '+ city + ' ' + petName)
+'''
+
