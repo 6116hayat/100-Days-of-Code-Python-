4fac48aee19b5e356b505694240b7a8822237305
Umar Hayat <umarhayat9718@gmail.com>
2024-02-21 12:32:39 +0530

    Adding Day 06: Python Functions

---
+++ Begginner/Day 06.py
@@ -0,0 +1,35 @@
+## Functions, Code Blocks and While Loops
+'''
+
+'''
+
+# 1. Functions()
+'''
+#Creating a function: 
+Code syntax
+def my_functions():
+    print('Hello Umar')
+    print('You are looking smart, as usual')
+    
+# Calling the functions
+my_functions()
+'''
+
+# Reeborg's World Exercise
+# website: https://www.reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Trial01&url=user_world%3ATrial01
+
+# 2. Indentation
+'''
+def my_function():
+....print('Hello')
+....print('World')
+print('Outside the functions')
+'''
+
+# 3. While Loops
+'''
+code syntax
+while something is true:
+    #Do something repeatedly
+'''
+
